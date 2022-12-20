dc2e0a4ed19d4616ac6d3f787b7745b0d3099ff6
“Omar <omaryassin552@gmail.com>
2022-12-19 20:52:09 -0700

    just started this shit yktv

--- Business Website/contact.php
+++ Business Website/contact.php
@@ -20,7 +20,7 @@ if (!$comment) $errors[count($errors)] = 'Please enter your message.';
 if (!$errors) {
 
 	//recipient - replace your email here
-	$to = 'emailaddress@gmail.com';	
+	$to = 'omaryassin552@gmail.com';	
 	//sender - from the form
 	$from = $name . ' <' . $email . '>';
 	
